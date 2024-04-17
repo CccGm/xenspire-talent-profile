26680952d6e239c935bdb05207618100249a8f3a
Ccc GM <mihirghelani.cyber@gmail.com>
2024-04-17 14:10:26 +0530

    selection screen change

--- src/components/dashboard/selection_screen.js
+++ src/components/dashboard/selection_screen.js
@@ -1,14 +1,104 @@
 import React from "react";
-import { HelpOutlineOutlined } from "@mui/icons-material";
+import {
+  HelpOutlineOutlined,
+  InsertChartOutlinedTwoTone,
+  LayersOutlined,
+  PeopleAltOutlined,
+} from "@mui/icons-material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Avatar, AvatarGroup } from "@mui/material";
-import { NavBar } from "../widgets/navbar";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+} from "@mui/material";
 import { Footer } from "../widgets/footer";
-import { SideNav } from "../widgets/sidenav";
+import { styled, useTheme } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItemText from "@mui/material/ListItemText";
+import logo from "../../assets/images/Logo.png";
+
+const drawerWidth = 250;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  marginBottom: 10,
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 export const Selection_Screen = () => {
   const data = [
@@ -90,6 +180,16 @@ export const Selection_Screen = () => {
       id: 342,
     },
   ];
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   const Avatar_Imgs = () => {
     return (
@@ -100,11 +200,11 @@ export const Selection_Screen = () => {
         }}>
         <Avatar
           alt="Cindy Baker"
-          src="https://avatar.iran.liara.run/public/boy?username=Ash"
+          src="https://eu.ui-avatars.com/api/?name=Ghelani+Mihir&size=250"
         />
         <Avatar
           alt="Agnes Walker"
-          src="https://avatar.iran.liara.run/public/boy?username=Ash"
+          src="https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=mail@ashallendesign.co.uk"
         />
         <Avatar
           alt="Trevor Henderson"
@@ -130,14 +230,90 @@ export const Selection_Screen = () => {
     );
   };
 
+  const drawer_Data = [
+    { name: "Jobs", icon: <InsertChartOutlinedTwoTone /> },
+    { name: "Candidates", icon: <LayersOutlined /> },
+    { name: "Customers", icon: <PeopleAltOutlined /> },
+  ];
+
   return (
-    <div>
-      <SideNav />
-      <div className="pt-32 pl-72 grid grid-flow-row mx-20">
+    <div className="flex">
+      <div>
+        <AppBar
+          position="fixed"
+          open={open}
+          sx={{ backgroundColor: "#B2D8D8" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 5,
+                ...(open && { display: "none" }),
+              }}>
+              <MenuIcon />
+            </IconButton>
+            <div className="grid grid-flow-col items-center justify-center">
+              <img src={logo} alt="logo" width={60} />
+              <text className="text-app-Teal font-extrabold text-2xl">
+                Xenspire Talent
+              </text>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader sx={{ backgroundColor: "#B2D8D8" }}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <div className="h-full bg-app-LightTeal pt-10">
+            <List>
+              {drawer_Data.map((data, index) => (
+                <ListItemButton
+                  key={index}
+                  sx={{
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
+                    backgroundColor: "white",
+                    margin: 1,
+                    borderRadius: 2,
+                  }}>
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                    }}>
+                    {data.icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={data.name}
+                    sx={{ opacity: open ? 1 : 0 }}
+                  />
+                </ListItemButton>
+              ))}
+            </List>
+          </div>
+          <Footer />
+        </Drawer>
+      </div>
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
         <div className="border-app-Teal border rounded-md p-3  grid grid-flow-col justify-between px-10">
           <text className="text-app-Teal font-semibold text-xl">Jobs</text>
           <HelpOutlineOutlined sx={{ color: "#98A2B3" }} />
         </div>
+
+        {/* table */}
         <div className="my-5  mb-16 border border-app-LightTeal rounded-md">
           <Table aria-label="simple table">
             <TableHead>
@@ -174,9 +350,7 @@ export const Selection_Screen = () => {
             </TableBody>
           </Table>
         </div>
-      </div>
-      <NavBar show={true} />
-
+      </Box>
       <Footer />
     </div>
   );
--- src/components/widgets/sidenav.js
+++ src/components/widgets/sidenav.js
@@ -4,12 +4,19 @@ import {
   LayersOutlined,
   PeopleAltOutlined,
 } from "@mui/icons-material";
+import logo from "../../assets/images/Logo.png";
 
 export const SideNav = () => {
   return (
-    <aside className="fixed h-screen w-72 bg-app-Border grid pt-44">
+    <aside className="fixed h-screen w-72 bg-app-LightTeal grid content-start gap-20 pt-10">
+      <div className="grid grid-flow-col items-center justify-center">
+        <img src={logo} alt="logo" width={60} />
+        <text className="text-app-Teal font-extrabold text-2xl">
+          Xenspire Talent
+        </text>
+      </div>
       <div>
-        <div className="bg-white m-2 grid-flow-col grid justify-between p-2 rounded-md">
+        <div className="bg-white m-2 grid-flow-col grid justify-start p-2 rounded-md">
           <div className="grid-flow-col grid">
             <InsertChartOutlinedTwoTone />
             <text style={{ color: "black", fontSize: 14, marginLeft: 10 }}>
@@ -17,7 +24,7 @@ export const SideNav = () => {
             </text>
           </div>
         </div>
-        <div className="bg-white m-2 grid-flow-col grid justify-between p-2 rounded-md">
+        <div className="bg-white m-2 grid-flow-col grid justify-start p-2 rounded-md">
           <div className="grid-flow-col grid">
             <LayersOutlined />
             <text style={{ color: "black", fontSize: 14, marginLeft: 10 }}>
@@ -25,7 +32,7 @@ export const SideNav = () => {
             </text>
           </div>
         </div>
-        <div className="bg-white m-2 grid-flow-col grid justify-between p-2 rounded-md">
+        <div className="bg-white m-2 grid-flow-col grid justify-start p-2 rounded-md">
           <div className="grid-flow-col grid">
             <PeopleAltOutlined />
             <text style={{ color: "black", fontSize: 14, marginLeft: 10 }}>
